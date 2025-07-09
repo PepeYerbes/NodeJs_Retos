cd47d62dbe0e7e7a51dd7830370c900514e57c5b
Rodrigo Leaños Bermejo <rodrigoBermejo@users.noreply.github.com>
2025-07-08 21:57:00 -0600

    update server.js

--- 02.Doing/semana7/src/controllers/usuarioController.js
+++ 02.Doing/semana7/src/controllers/usuarioController.js
@@ -4,7 +4,7 @@ async function obtenerUsuario(req, res) {
   try {
     const user = await Usuario.findById(req.params.id);
     if (!user) {
-      res.status(400).json({ error: 'Usuario no encontrado' });
+      return res.status(400).json({ error: 'Usuario no encontrado' });
     }
     res.json(user);
   } catch (error) {
@@ -25,7 +25,7 @@ async function crearUsuario(req, res) {
   try {
     const { nombreCompleto, email, municipioId } = req.body;
     if (!nombreCompleto || !email || !municipioId) {
-      res.status(400).json({ error: 'Faltan campos requeridos' });
+      return res.status(400).json({ error: 'Faltan campos requeridos' });
     }
     const newUser = await Usuario.create({ nombreCompleto, email, municipioId });
     res.json(newUser);
@@ -38,12 +38,12 @@ async function actualizarUsuario(req, res) {
   try {
     const { nombreCompleto, email, municipioId } = req.body;
     if (!nombreCompleto || !email || !municipioId) {
-      res.status(400).json({ error: 'Faltan campos requeridos' });
+      return res.status(400).json({ error: 'Faltan campos requeridos' });
     }
     const updatedUser = await Usuario.findByIdAndUpdate(req.params.id, req.body);
 
     if (!updatedUser) {
-      res.status(400).json({ error: 'Usuario no encontrado' });
+      return res.status(400).json({ error: 'Usuario no encontrado' });
     }
     res.json(updatedUser);
 
@@ -57,7 +57,7 @@ async function eliminarUsuario(req, res) {
     const deletedUser = await Usuario.findByIdAndDelete(req.params.id);
 
     if (!deletedUser) {
-      res.status(400).json({ error: 'Usuario no encontrado' });
+      return res.status(400).json({ error: 'Usuario no encontrado' });
     }
     res.status(204);
   } catch (error) {
